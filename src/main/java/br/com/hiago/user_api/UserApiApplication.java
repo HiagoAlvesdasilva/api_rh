4bc59b85557b073e29b8fd6bbd6ce84fe84a1170
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-14 22:55:43 -0300

    feat: adiciona User e UserRepository

--- src/main/java/br/com/hiago/user_api/UserApiApplication.java
+++ src/main/java/br/com/hiago/user_api/UserApiApplication.java
@@ -1,13 +1,29 @@
 package br.com.hiago.user_api;
 
+import br.com.hiago.user_api.domain.User;
+import br.com.hiago.user_api.repositories.UserRepositiry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class UserApiApplication {
+public class UserApiApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepositiry userRepositiry;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        userRepositiry.saveAll(List.of(
+                new User(null,"Teste", "email@teste", "senha123",20)
+        ));
+
+    }
 }
--- src/main/java/br/com/hiago/user_api/domain/User.java
+++ src/main/java/br/com/hiago/user_api/domain/User.java
@@ -1,4 +1,32 @@
 package br.com.hiago.user_api.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "TB_USER")
 public class User {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private double hourlyPrice;
+
+    public User(Long id, String name, String email, String password, double hourlyPrice) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.hourlyPrice = hourlyPrice;
+    }
+    public User() {
+    }
 }
