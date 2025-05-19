680b271852483151a939ab0695f5b1311192f282
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-18 21:18:26 -0300

    feat: criado UserResource

--- src/main/java/br/com/hiago/user_api/resources/UserResource.java
+++ src/main/java/br/com/hiago/user_api/resources/UserResource.java
@@ -1,4 +1,17 @@
 package br.com.hiago.user_api.resources;
 
+import br.com.hiago.user_api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public interface UserResource {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<User> findById(@PathVariable Long id);
+
+    @GetMapping
+    ResponseEntity<List<User>> findAll();
 }
