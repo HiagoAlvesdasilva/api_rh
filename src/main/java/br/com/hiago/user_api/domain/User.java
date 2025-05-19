26ed78194d6e396c40a26b5e9502f391c78c536a
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-18 21:16:28 -0300

    adjust: ajustado metodos geters e seters

--- src/main/java/br/com/hiago/user_api/domain/User.java
+++ src/main/java/br/com/hiago/user_api/domain/User.java
@@ -1,23 +1,31 @@
 package br.com.hiago.user_api.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Getter @Setter
+@ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity(name = "TB_USER")
 public class User {
 
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "hourlyPrice")
     private double hourlyPrice;
 
     public User(Long id, String name, String email, String password, double hourlyPrice) {
@@ -29,4 +37,46 @@ public class User {
     }
     public User() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getHourlyPrice() {
+        return hourlyPrice;
+    }
+
+    public void setHourlyPrice(double hourlyPrice) {
+        this.hourlyPrice = hourlyPrice;
+    }
+
+
 }
