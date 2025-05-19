900de26a07598c7a2d5461b29778749d8258bf34
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-18 21:09:49 -0300

    adjust: criado usuarios para testar

--- src/main/java/br/com/hiago/user_api/UserApiApplication.java
+++ src/main/java/br/com/hiago/user_api/UserApiApplication.java
@@ -1,7 +1,7 @@
 package br.com.hiago.user_api;
 
 import br.com.hiago.user_api.domain.User;
-import br.com.hiago.user_api.repositories.UserRepositiry;
+import br.com.hiago.user_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +13,7 @@ import java.util.List;
 public class UserApiApplication implements CommandLineRunner {
 
     @Autowired
-    private UserRepositiry userRepositiry;
+    private UserRepository userRepositiry;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
@@ -22,7 +22,8 @@ public class UserApiApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         userRepositiry.saveAll(List.of(
-                new User(null,"Teste", "email@teste", "senha123",20)
+                new User(null,"Teste", "email@teste", "senha123",20),
+                new User(null,"Teste01", "email@teste", "senha123",29)
         ));
 
     }
