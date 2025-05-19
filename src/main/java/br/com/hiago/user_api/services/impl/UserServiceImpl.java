1fad31f463219b42abadcbce55a9653e23437859
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-18 21:20:58 -0300

    adjust: implementado metodos no UserServiceImpl

--- src/main/java/br/com/hiago/user_api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/hiago/user_api/services/impl/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package br.com.hiago.user_api.services.impl;
 
-public class UserServiceImpl {
+import br.com.hiago.user_api.domain.User;
+import br.com.hiago.user_api.repositories.UserRepository;
+import br.com.hiago.user_api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<User> users = userRepository.findAll();
+        System.out.println("Usuarios no banco: "+ users);
+        return users;
+//        return userRepository.findAll();
+    }
 }
