f32c202e6e3d72a41de4de27f1c7474f2b08098e
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-18 21:20:30 -0300

    adjust: implementado metodos no UserService

--- src/main/java/br/com/hiago/user_api/services/UserService.java
+++ src/main/java/br/com/hiago/user_api/services/UserService.java
@@ -1,4 +1,13 @@
 package br.com.hiago.user_api.services;
 
-public class UserService {
+import br.com.hiago.user_api.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public interface UserService {
+
+    User findById(Long id);
+    List<User>findAll();
+
 }
